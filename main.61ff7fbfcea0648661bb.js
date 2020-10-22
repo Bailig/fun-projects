ca5c9abf7dab790bb7edb1677b4c25e02f980287
Bailig <bailig.bai@ef.com>
2020-10-22 12:30:28 +0800

    [skip ci] Updates

---
+++ main.61ff7fbfcea0648661bb.js
@@ -0,0 +1,19 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{39:function(e,a,t){e.exports=t(49)},49:function(e,a,t){"use strict";t.r(a);var l=t(0),n=t.n(l),r=t(30),c=t.n(r),o=t(27),m=t(18),u=t.p+"66b65d249f00bdd65b6a062d7ef49d04.ttf";const i=m.b`
+  @font-face {
+    font-family: OzCaramel;
+    src: url(${u});
+  }
+
+  body {
+    margin: 0;
+    font-family: OzCaramel, sans-serif;
+  }
+
+  button {
+    background-color: transparent;
+    outline: none;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+  }
+`;var E=t(11),b=t(3);const p=n.a.lazy(()=>t.e(8).then(t.bind(null,68))),s=n.a.lazy(()=>Promise.all([t.e(0),t.e(5)]).then(t.bind(null,63))),d=n.a.lazy(()=>t.e(6).then(t.bind(null,65))),h=n.a.lazy(()=>t.e(3).then(t.bind(null,64))),f=n.a.lazy(()=>t.e(7).then(t.bind(null,66))),y=n.a.lazy(()=>t.e(4).then(t.bind(null,67))),v=()=>n.a.createElement(n.a.Fragment,null,n.a.createElement(E.b,{to:"/quotes"},"quotes"),n.a.createElement("br",null),n.a.createElement(E.b,{to:"/infinite-scroll"},"infinite-scroll"),n.a.createElement("br",null),n.a.createElement(E.b,{to:"/joke-teller"},"joke-teller"),n.a.createElement("br",null),n.a.createElement(E.b,{to:"/music-player"},"music-player"),n.a.createElement("br",null),n.a.createElement(E.b,{to:"/paint-clone"},"paint-clone"),n.a.createElement("br",null),n.a.createElement(E.b,{to:"/video-player"},"video-player")),z=()=>n.a.createElement(l.Suspense,{fallback:n.a.createElement("div",null,"Loading...")},n.a.createElement(E.a,null,n.a.createElement(b.c,null,n.a.createElement(b.a,{path:"/",exact:!0},n.a.createElement(v,null)),n.a.createElement(b.a,{path:"/quotes"},n.a.createElement(p,null)),n.a.createElement(b.a,{path:"/infinite-scroll"},n.a.createElement(s,null)),n.a.createElement(b.a,{path:"/joke-teller"},n.a.createElement(d,null)),n.a.createElement(b.a,{path:"/music-player"},n.a.createElement(h,null)),n.a.createElement(b.a,{path:"/paint-clone"},n.a.createElement(f,null)),n.a.createElement(b.a,{path:"/video-player"},n.a.createElement(y,null)))));var k=t(55),w=t(58);const g=Object(k.a)(),j=({theme:e=g,children:a})=>n.a.createElement(w.a,{theme:e},n.a.createElement(m.a,{theme:e},a)),O=document.querySelector("#app");c.a.render(n.a.createElement(()=>n.a.createElement(j,null,n.a.createElement(o.a,{value:{revalidateOnFocus:!1,revalidateOnReconnect:!1,fetcher:(...e)=>fetch(...e).then(e=>e.json())}},n.a.createElement(i,null),n.a.createElement(z,null))),null),O)}},[[39,2,0]]]);
